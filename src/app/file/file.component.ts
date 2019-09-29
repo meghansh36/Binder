44dcd3726c6213c425bad7ff8212d793ba6b91d2
meghansh36 <meghansh.work@gmail.com>
2019-09-29 22:40:41 +0530

    comments added

--- src/app/file/file.component.ts
+++ src/app/file/file.component.ts
@@ -4,45 +4,79 @@ import { MatMenuTrigger } from '@angular/material/menu';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { RenameSheetComponent } from '../rename-sheet/rename-sheet.component';
 
+/**
+ * Component for displaying each system file
+ */
 @Component({
-  // tslint:disable-next-line: component-selector
   selector: 'file',
   templateUrl: './file.component.html',
   styleUrls: ['./file.component.css'],
   providers: [SystemService]
 })
 export class FileComponent implements OnInit {
+  /**
+   * trigger for menu button
+   */
   @ViewChild('trigger', {read: MatMenuTrigger, static: false}) trigger: MatMenuTrigger;
-  // tslint:disable-next-line: no-input-rename
+
+  /**
+   * The file metadata object for this file.
+   */
   @Input('fileInfo') file: object;
+
+  /**
+   * Preview image base64 string
+   */
   imageToShow: any;
+  /**
+   * whether to show proview or show loader. Initally set to show loader.
+   */
   showPreview = false;
   timer: NodeJS.Timer;
+
+  /**
+   * Last modified date string of the file
+   */
   LMDate: string;
   constructor(private systemService: SystemService, private cd: ChangeDetectorRef, private _bottomSheet: MatBottomSheet) { }
 
+
   ngOnInit() {
-    // show loader
-    // call generate preview function
+    // generate date from the modified time in milliseconds.
     let date = new Date(this.file["stat"].mtimeMs);
     this.LMDate = `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`
+    // call generate preview function
     this.generateFilePreview();
   }
   
+  /**
+   * This function fetches the files preview
+   */
   generateFilePreview() {
+    // ste timer to wait for 80 seconds to fetch preview. If preview not fetched within 80s, error image is shown.
     this.timer = setTimeout(() => {
+      // show error image
       this.imageToShow = this.systemService.errorImg;
       this.showPreview = true;
       this.cd.detectChanges();  
     }, 80000);
 
+
     this.systemService.getFilePreview(this.file['filename']);
+    /**
+     * Subscribe to the preview emitter observable
+     */
     this.systemService.previewEmitter.subscribe(data => {
       this.renderPreview(data);
     })
   }
 
+  /**
+   * Render the preview image of the file
+   * @param data : base64 string of the image
+   */
   renderPreview(data) {
+    // clear the timer on successful fetch of preview image
     clearTimeout(this.timer);
     this.imageToShow = data;
     this.showPreview = true;
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { SystemService } from '../services/system.service';
 import { DriveService } from '../services/drive.service';
 
+/**
+ * This component is the main home view component
+ */
 @Component({
-  // tslint:disable-next-line: component-selector
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
@@ -11,54 +13,95 @@ import { DriveService } from '../services/drive.service';
 })
 export class HomeComponent implements OnInit {
 
+  /**
+   * array that stores the metadata of files found on the system
+   */
   systemFiles = [];
+
+  /**
+   * array that stores the metadata of files found on google drive
+   */
   driveFiles: Array<object>;
+
+  /**
+   * array that stores top 10 recently used files
+   */
   recentDriveFiles: Array<object>;
+
+  /**
+   * status of the google login
+   */
   googleDriveLogInStatus: boolean;
+
   constructor(private systemService: SystemService, private cd: ChangeDetectorRef, private driveService: DriveService) { }
 
 
   ngOnInit() {
-
-    this.systemService.systemFileEmitter.subscribe((fileData: Array<object>) => {
-      this.receivedSystemFiles(fileData);
-    });
-
+    // check google login status
     this.checkGoogleLogin();
+    // fetch system files
     this.fetchSystemFiles();
   }
 
+  /**
+   * Initiate google login procedure
+   */
   googleLogin() {
     this.driveService.googleLogin();
     this.driveService.googleLoginEvent.subscribe(success => {
       if (success) {
         this.googleDriveLogInStatus = true;
+        // fetch drive files on successful login
         this.fetchDriveFiles();
       }
     });
   }
 
+  /**
+   * Check the status of google login
+   */
   checkGoogleLogin() {
     this.googleDriveLogInStatus = this.driveService.checkGoogleLogin();
-    this.fetchDriveFiles();
+    if(this.googleDriveLogInStatus)
+      this.fetchDriveFiles();
+  }
+  
+  /**
+   * fetches drive files from the proxy server
+   */
+  fetchDriveFiles() {
+    this.driveService.fetchDriveFiles();
+    // subscribe to the ftehc drive files observable
     this.driveService.fetchDriveFilesEvent.subscribe((files: Array<object>) => {
       this.driveFiles = files;
       this.recentDriveFiles = this.driveFiles.slice(0, 10);
+      // udpdate the Angular DOM explicitly
       this.cd.detectChanges();
     });
   }
 
-  fetchDriveFiles() {
-    this.driveService.fetchDriveFiles();
-  }
-
+  /**
+   * Fetch files from the system
+   */
   fetchSystemFiles() {
     this.systemService.fetchSystemFiles();
+
+    /**
+     * Subscribes to the fetch system files observable
+     */
+    this.systemService.systemFileEmitter.subscribe((fileData: Array<object>) => {
+      this.receivedSystemFiles(fileData);
+    });
   }
 
+  /**
+   * Fires after the system files are fetched
+   * @param files : Array of objects for files received
+   */
   receivedSystemFiles(files: Array<object>) {
     this.systemFiles = files;
     console.log(this.systemFiles);
+    // udpdate the Angular DOM explicitly    
     this.cd.detectChanges();
   }
 
